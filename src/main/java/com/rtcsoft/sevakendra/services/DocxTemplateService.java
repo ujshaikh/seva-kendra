7f62244d52e7a528769a20b58820428269d53e17
Usman J Shaikh <107400657+ujshaikh@users.noreply.github.com>
2024-11-18 17:35:03 +0530

    Minor changes

--- src/main/java/com/rtcsoft/sevakendra/services/DocxTemplateService.java
+++ src/main/java/com/rtcsoft/sevakendra/services/DocxTemplateService.java
@@ -111,26 +111,31 @@ public class DocxTemplateService {
 	 * @param fileList The list to store file paths
 	 */
 	public static void scanDirectory(File folder, List<String> fileList) {
-		System.out.println("SCANNING_STARTED_IN_RECUSRSIVE_WAY");
-		if (folder.exists() && folder.isDirectory()) {
-			System.out.println("CHECKED_FOLDER_EXISTS");
-			File[] files = folder.listFiles();
-			System.out.println(files);
-			if (files != null) {
-				for (File file : files) {
-					if (file.isFile()) {
-						// Add file name to the list
-						System.out.println("ADDING_FILE_TO_THE_LIST");
-						System.out.println(file.getName());
-						fileList.add(file.getName());
-					} else if (file.isDirectory()) {
-						// Recurse into sub directory
-						scanDirectory(file, fileList);
+		try {
+			System.out.println("SCANNING_STARTED_IN_RECUSRSIVE_WAY");
+			if (folder.exists() && folder.isDirectory()) {
+				System.out.println("CHECKED_FOLDER_EXISTS");
+				File[] files = folder.listFiles();
+				System.out.println(files);
+				if (files != null) {
+					for (File file : files) {
+						if (file.isFile()) {
+							// Add file name to the list
+							System.out.println("ADDING_FILE_TO_THE_LIST");
+							System.out.println(file.getName());
+							fileList.add(file.getName());
+						} else if (file.isDirectory()) {
+							// Recurse into sub directory
+							scanDirectory(file, fileList);
+						}
 					}
 				}
+			} else {
+				System.out.println("The folder does not exist or is not a directory: " + folder.getAbsolutePath());
 			}
-		} else {
-			System.out.println("The folder does not exist or is not a directory: " + folder.getAbsolutePath());
+		} catch (Exception e) {
+			e.printStackTrace();
+			System.out.println(e.getMessage());
 		}
 	}
 
