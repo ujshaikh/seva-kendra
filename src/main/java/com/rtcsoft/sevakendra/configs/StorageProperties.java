6f50ee32c67ac7ba2d9f39d4f6de5eebd016f927
Usman J Shaikh <107400657+ujshaikh@users.noreply.github.com>
2024-11-24 19:36:15 +0530

    Getting upload file dir path from props

--- src/main/java/com/rtcsoft/sevakendra/configs/StorageProperties.java
+++ src/main/java/com/rtcsoft/sevakendra/configs/StorageProperties.java
@@ -1,16 +1,19 @@
 package com.rtcsoft.sevakendra.configs;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @ConfigurationProperties("storage")
 public class StorageProperties {
+	@Value("${spring.customers-uploads}")
+	private String uploadDir;
 
 	/**
 	 * Folder location for storing files
 	 */
-	private String location = "F:\\Clients\\Saeed Deshmukh\\Projects\\var\\uploads\\";
+	private String location = uploadDir != null ? uploadDir : "/var/sevakendra/uploads/";
 
 	public String getLocation() {
 		return location;
